9c284193e4e450dbffe4715d6a89ac83b73f3286
yy710 <171254868@qq.com>
2018-10-11 23:35:35 +0800

    save base64 txt file

--- routes/learning-english.js
+++ routes/learning-english.js
@@ -1,13 +1,27 @@
 let express = require('express');
 let router = express.Router();
-let multer  = require('multer');
-let upload = multer({ dest: '../uploads/' });
+let multer = require('multer');
+let upload = multer({dest: '../uploads/'});
+let fs = require('fs');
 
 /* GET home page. */
-router.post('/upload', upload.single('record'), function(req, res, next) {
+router.post('/upload', upload.single('record'), function (req, res, next) {
     //debug
-    //console.log(req.file);
-    console.log(req.body);
+    console.log("req.file: ", req.file);
+    console.log("req.body: ", req.body);
+
+    fs.readFile(req.file.path, (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            let buf = Buffer.from(data);
+            let base64 = buf.toString('base64');
+            fs.writeFile(req.file.destination + 'voice.txt', base64, err => {
+                if (err) throw err;
+            });
+        }
+    });
+
     res.json({test: 'ok!'});
 });
 
