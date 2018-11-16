4f64e3b7854dc88c1848696784b61a0e3dc83458
yy710 <171254868@qq.com>
2018-11-16 18:33:29 +0800

    ent

--- sentence.js
+++ sentence.js
@@ -1,10 +1,10 @@
 
 class Sentence{
     constructor(){
-        this.id = null;
-        this.text = null;
-        this.audio = null;
-        this.image = null;
+        this.id = 1;
+        this.text = "Help A monster said Annie Yeah sure said Jack A real monster in Frog Creek Pennsylvania";
+        this.audio = { src: "https://www.all2key.cn/learning-english/audio/01intoWoods.mp3", startTime: 21, endTime: 31.5};
+        this.image = "https://www.all2key.cn/learning-english/images/001.png";
     }
 }
 
